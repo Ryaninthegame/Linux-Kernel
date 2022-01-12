a4b3470d1d7efb85037fbe5fff0ecae5674faf3c
Ryan Wang <ryandothetest@gmail.com>
2022-01-13 00:15:26 +0800

    Update Kernel_space.c

--- Project_2/Kernel_space.c
+++ Project_2/Kernel_space.c
@@ -8,10 +8,10 @@
 
 static unsigned long vaddr2paddr(unsigned long vaddr){
     pgd_t *pgd;
+    p4d_t *p4d;
     pud_t *pud;
     pmd_t *pmd;
     pte_t *pte;
-    p4d_t *p4d;
 
     unsigned long paddr = 0;
     unsigned long page_addr = 0;
@@ -22,23 +22,23 @@ static unsigned long vaddr2paddr(unsigned long vaddr){
     pgd = pgd_offset(current->mm, vaddr);
     printk("pgd_val = 0x%lx\n", pgd_val(*pgd));
     printk("pgd_index = %lu\n", pgd_index(vaddr));
-    if (pgd_none(*pgd)) {
+    if (pgd_none(*pgd)){
         printk("not mapped in pgd\n");
         return -1;
     }
-
+    
     p4d = p4d_offset(pgd, vaddr);
     printk("p4d_val = 0x%lx\n", p4d_val(*p4d));
     printk("p4d_index = %lu\n", p4d_index(vaddr));
-    if (p4d_none(*p4d)) {
+    if (p4d_none(*p4d)){
         printk("not mapped in p4d\n");
         return -1;
     }
 
-
     pud = pud_offset(p4d, vaddr);
     printk("pud_val = 0x%lx\n", pud_val(*pud));
-    if (pud_none(*pud)) {
+    printk("pud_index = %lu\n", pud_index(vaddr));
+    if (pud_none(*pud)){
         printk("not mapped in pud\n");
         return -1;
     }
@@ -46,15 +46,15 @@ static unsigned long vaddr2paddr(unsigned long vaddr){
     pmd = pmd_offset(pud, vaddr);
     printk("pmd_val = 0x%lx\n", pmd_val(*pmd));
     printk("pmd_index = %lu\n", pmd_index(vaddr));
-    if (pmd_none(*pmd)) {
+    if (pmd_none(*pmd)){
         printk("not mapped in pmd\n");
         return -1;
     }
-
+    
     pte = pte_offset_kernel(pmd, vaddr);
     printk("pte_val = 0x%lx\n", pte_val(*pte));
     printk("pte_index = %lu\n", pte_index(vaddr));
-    if (pte_none(*pte)) {
+    if (pte_none(*pte)){
         printk("not mapped in pte\n");
         return -1;
     }
@@ -63,7 +63,7 @@ static unsigned long vaddr2paddr(unsigned long vaddr){
     page = pte_page(*pte);
     paddr = page_to_phys(page) + (vaddr & (~PAGE_MASK));
     printk("page_addr = %lx, page_offset = %lx\n", page_addr, page_offset);
-        printk("vaddr = %lx, paddr = %lx\n", vaddr, paddr);
+    printk("vaddr = %lx, paddr = %lx\n", vaddr, paddr);
 
     return paddr;
 }
